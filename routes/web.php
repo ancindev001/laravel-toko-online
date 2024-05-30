66b3062b8ae36dd85f4fab76e5032b2d56636c34
Iman N <manznurz@gmail.com>
2024-05-30 21:02:02 +0700

    feat(auth): update route permissions with can() middleware
    This commit updates the route permissions to use the 'can()' middleware instead of 'admin' middleware.
    This allows for more granular control over which users can access certain routes.
    
    BREAKING CHANGE: The 'admin' middleware has been replaced with the 'can()' middleware.
    This means that any routes that were previously protected by the 'admin' middleware will now need to be updated to use the 'can()' middleware.

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,8 @@ use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\OrderController;
-use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TransactionController;
-use App\Models\Transaction;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -28,33 +25,28 @@ Route::get('/', [HomeController::class, 'index']);
 
 Auth::routes();
 
-
-
-
-Route::prefix('admin')->middleware('admin')->group(function () {
+Route::prefix('admin')->middleware('can:isAdmin')->group(function () {
     Route::resource('customer', CustomerController::class);
     Route::resource('product', ProductController::class);
 });
 
-
-
 Route::middleware('auth')->group(function () {
-
+    Route::get("/dashboard", [DashboardController::class, 'index'])->name('dashboard');
     //profile
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
     Route::post('/profile/{user}/update', [ProfileController::class, 'update'])->name('profile.update');
-
-    // cart
-    Route::get("cart", [CartController::class, 'index'])->name('cart.index');
-    Route::put("cart", [CartController::class, 'update'])->name('cart.update');
-    Route::get("/cart/{product}", [CartController::class, 'store'])->name('cart.create');
     // order
     Route::get('/order', [OrderController::class, 'index'])->name('order.index');
     Route::get('/order/store', [OrderController::class, 'store'])->name('order.store');
     Route::get('/order/{order}/show', [OrderController::class, 'show'])->name('order.show');
     Route::post('/order/{order}/update', [OrderController::class, 'update'])->name('order.update');
+});
 
+Route::middleware('can:isCustomer')->group(function () {
+    // cart
+    Route::get("cart", [CartController::class, 'index'])->name('cart.index');
+    Route::put("cart", [CartController::class, 'update'])->name('cart.update');
+    Route::get("/cart/{product}", [CartController::class, 'store'])->name('cart.create');
     // checkout
     Route::get('/checkout', [CartController::class, 'checkout'])->name('checkout.index');
-    Route::get("/dashboard", [DashboardController::class, 'index'])->name('dashboard');
 });
